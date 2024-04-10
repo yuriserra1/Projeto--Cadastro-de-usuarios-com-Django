21b1fd4533f0fb7cab4143f1db4994230f63864f
Yuri Oliveira Serra <yurisnuky@gmail.com>
2024-04-10 20:53:23 -0300

    initial commit

---
+++ projeto_cad_usuarios/app_cad_usuarios/views.py
@@ -0,0 +1,17 @@
+from django.shortcuts import render
+from .models import Usuario
+def home(request):
+    return render(request,"usuarios/home.html")
+
+def usuarios(request):
+    #Salvar os dados da tela para o banco de dados.
+    novo_usuario = Usuario()
+    novo_usuario.nome = request.POST.get("nome")
+    novo_usuario.idade = request.POST.get("idade")
+    novo_usuario.save()
+    # Exibir todos os usuarios ja cadastrados em uma nova página
+    usuarios = {
+        "usuarios": Usuario.objects.all()
+    }
+    # Retornar os dados para a pagina do usuario
+    return render(request,"usuarios/usuarios.html",usuarios)
